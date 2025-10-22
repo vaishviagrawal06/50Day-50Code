5902b9f4b1bdca24fb887ebad33620c3a78d5446
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2025-10-22 21:47:09 +0530

    msg

---
+++ code7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int kadanse_algo(int arr[], int n) {
+    int maxsummarr = arr[0];
+    int currsum = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (currsum < 0) {
+            currsum = 0;
+        }
+        currsum += arr[i];
+        if (currsum > maxsummarr) {
+            maxsummarr = currsum;
+        }
+    }
+    return maxsummarr;
+}
+int main() {
+    int arr[] = {-3,-5,10,-7,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int max_sum = kadanse_algo(arr, n);
+
+    printf("Max subarray sum is %d\n", max_sum);
+
+    return 0;
+}
